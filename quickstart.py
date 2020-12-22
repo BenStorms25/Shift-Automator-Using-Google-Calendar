443bfe4018324753f912f1f89aa3121ea8cb1820
BenStorms25 <65511280+BenStorms25@users.noreply.github.com>
2020-12-22 16:08:13 -0500

    Working Event Adder
    Adds Events Effectively.  Now All that needs to happen in this file is creating a function that takes in dates and creates the event.

--- quickstart.py
+++ quickstart.py
@@ -30,3 +30,33 @@ def deleteCalendar(service, IdString):
     """
 
     service.calendars().delete(calendarId=IdString).execute()
+
+event = {
+  'summary': 'Final Exam',
+  
+  'description': 'It\'s the exam',
+  'start': {
+    'dateTime': '2020-12-24T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'end': {
+    'dateTime': '2020-12-24T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'},
+  ],
+  'reminders': {
+    'useDefault': False,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+}
+
+event = service.events().insert(calendarId='primary', body=event).execute()
