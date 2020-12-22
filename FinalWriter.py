941c27eb2d542018cf2cd59214edba47c4a6a694
BenStorms25 <65511280+BenStorms25@users.noreply.github.com>
2020-12-22 14:03:05 -0500

    Working Test (Add and Delete Calendars).
    Finally got it to work.  Uses a separate file called Google.py to create service objects that handle the communication to server.

--- FinalWriter.py
+++ FinalWriter.py
@@ -1,2 +1,4 @@
 # this is a program that will take a pdf of a course syllabus, find the final date, and write that to Google Calendar.
 
+import quickstart
+
--- quickstart.py
+++ quickstart.py
@@ -1,54 +1,32 @@
-from __future__ import print_function
-import datetime
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from pprint import pprint
+from Google import Create_Service
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+CLIENT_SECRET_FILE = "Client_Secret_New.json"
+API_NAME = 'calendar'
+API_VERSION = 'v3'
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
 
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
+def createCalendar(service, Name):
+
+    """ 
+    Creates a calendar.
+
     """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
 
-    service = build('calendar', 'v3', credentials=creds)
+    request_body = {
+    'summary': Name
+    }
+    response = service.calendars().insert(body = request_body).execute()
+    print(response)
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
+def deleteCalendar(service, IdString):
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    """
+    Deletes a calendar by ID.
 
+    """
 
-if __name__ == '__main__':
-    main()
+    service.calendars().delete(calendarId=IdString).execute()
