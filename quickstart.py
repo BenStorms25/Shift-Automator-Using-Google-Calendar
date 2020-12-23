a280b1e8e6691affe30975462f2d790ee07a0e51
BenStorms25 <65511280+BenStorms25@users.noreply.github.com>
2020-12-23 08:50:29 -0500

    Create Event Function
    created a function in the quickstart file that creates an event using optional parameters.

--- quickstart.py
+++ quickstart.py
@@ -20,7 +20,9 @@ def createCalendar(service, Name):
     'summary': Name
     }
     response = service.calendars().insert(body = request_body).execute()
+    calID = response['id']
     print(response)
+    return calID
 
 def deleteCalendar(service, IdString):
 
@@ -31,32 +33,68 @@ def deleteCalendar(service, IdString):
 
     service.calendars().delete(calendarId=IdString).execute()
 
-event = {
-  'summary': 'Final Exam',
+# event = {
+#   'summary': 'Final Exam',
   
-  'description': 'It\'s the exam',
-  'start': {
-    'dateTime': '2020-12-24T09:00:00-07:00',
-    'timeZone': 'America/Los_Angeles',
-  },
-  'end': {
-    'dateTime': '2020-12-24T17:00:00-07:00',
-    'timeZone': 'America/Los_Angeles',
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=2'
-  ],
-  'attendees': [
-    {'email': 'lpage@example.com'},
-    {'email': 'sbrin@example.com'},
-  ],
-  'reminders': {
-    'useDefault': False,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10},
-    ],
-  },
-}
+#   'description': 'It\'s the exam',
+#   'start': {
+#     'dateTime': '2020-12-29T04:00:00-07:00',
+#     'timeZone': 'America/New_York',
+#   },
+#   'end': {
+#     'dateTime': '2020-12-29T5:00:00-07:00',
+#     'timeZone': 'America/New_York',
+#   },
+#   'recurrence': [
+#     'RRULE:FREQ=DAILY;COUNT=1'
+#   ],
+#   'attendees': [
+#     {'email': 'lpage@example.com'},
+#     {'email': 'sbrin@example.com'},
+#   ],
+#   'reminders': {
+#     'useDefault': False,
+#     'overrides': [
+#       {'method': 'email', 'minutes': 24 * 60},
+#       {'method': 'popup', 'minutes': 10},
+#     ],
+#   },
+# }
 
-event = service.events().insert(calendarId='primary', body=event).execute()
+# service.events().insert(calendarId='mhj5hgpvpjs6g7po8pvj3r7q6c@group.calendar.google.com', body=event).execute()
+#event = 
+def insertEvent(service, calendarId, startDate = '2020-12-29', endDate = '2020-12-29', startTime = '04:00:00-07:00', endTime = '5:00:00-07:00'):
+  event = {
+    'summary': 'Final Exam',
+  
+    'description': 'It\'s the exam',
+    'start': {
+      'dateTime': startDate + 'T' + startTime,
+      'timeZone': 'America/New_York',
+    },
+    'end': {
+      'dateTime': '2020-12-29T5:00:00-07:00',
+      'timeZone': 'America/New_York',
+    },
+    'recurrence': [
+      'RRULE:FREQ=DAILY;COUNT=1'
+    ],
+    'attendees': [
+      {'email': 'lpage@example.com'},
+      {'email': 'sbrin@example.com'},
+    ],
+    'reminders': {
+      'useDefault': False,
+      'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+      ],
+    },
+  }
+
+  service.events().insert(calendarId='mhj5hgpvpjs6g7po8pvj3r7q6c@group.calendar.google.com', body=event).execute()
+
+
+
+calID = createCalendar(service, "Final Dates")
+insertEvent(service, calID,startDate = '2020-12-29',endDate='2020-12-29',)
