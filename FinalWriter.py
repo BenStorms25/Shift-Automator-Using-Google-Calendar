9b5488611670d30392aa503be8831e365fcca437
BenStorms25 <65511280+BenStorms25@users.noreply.github.com>
2020-12-27 17:32:48 -0500

    Finished Work
    Got it to work today to allow me to enter my shifts into google calendar.

--- FinalWriter.py
+++ FinalWriter.py
@@ -2,3 +2,57 @@
 
 import quickstart
 
+class Shift:
+    def __init__(self, date, start, end):
+        self.date = date
+        self.start = start
+        self.end = end
+
+
+def main():
+
+
+    #creates service for google calendar to be written to.
+    service = quickstart.createService()
+
+    #beginning message
+    print("Hello, this is to help you stay organized with your shift throughout the week.")
+
+    #creates list of shifts that will be written to.
+    listofShifts = []
+    shiftInfoString = input("Enter the shift in the form: yyyy-mm-dd Start End, with spaces in between: ")
+    listofShifts.append(shiftInfoString)
+
+    while(shiftInfoString != ''):
+
+        shiftInfoString = input("Enter the shift: ")
+        listofShifts.append(shiftInfoString)
+
+    #remove last element of listOfShifts (should be '')
+    listofShifts.pop()
+
+    listOfShiftObj = []
+
+    for shift in listofShifts:
+        shiftList = shift.split()
+        thisShift = Shift(shiftList[0], shiftList[1], shiftList[2])
+        listOfShiftObj.append(thisShift)
+    
+    print(listOfShiftObj)
+
+    listOfShiftObj = quickstart.formatTime(listOfShiftObj)
+    for shift in listOfShiftObj:
+        print(shift.start)
+
+
+    calID = quickstart.createCalendar(service, "Work Shifts")
+    
+    for shiftObj in listOfShiftObj:
+        quickstart.insertEvent(service = service, calendarId = calID, startDate = shiftObj.date, endDate = shiftObj.date, startTime = shiftObj.start, endTime = shiftObj.end)
+
+main()
+
+
+
+
+
--- quickstart.py
+++ quickstart.py
@@ -1,13 +1,48 @@
 from pprint import pprint
 from Google import Create_Service
 
-CLIENT_SECRET_FILE = "Client_Secret_New.json"
-API_NAME = 'calendar'
-API_VERSION = 'v3'
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
+def createService():
+  CLIENT_SECRET_FILE = "C:\Dev\FinalWriter\Final-Writer\Client_Secret_New.json"
+  API_NAME = 'calendar'
+  API_VERSION = 'v3'
+  SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+  service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
+  return service
+
+def formatTime(listOfShiftObj):
+
+    for shift in listOfShiftObj:
+
+        if "pm" in shift.start:
+            timeString = shift.start
+            timeString = timeString.replace("pm","")
+            timeString = timeString.split(":")
+            timeInt = int(timeString[0]) + 12
+            newTimeString = (str(timeInt) + ":" + timeString[1])
+            shift.start = newTimeString
+
+        if "pm" in shift.end:
+            timeString = shift.end
+            timeString = timeString.replace("pm","")
+            timeString = timeString.split(":")
+            timeInt = int(timeString[0]) + 12
+            newTimeString = (str(timeInt) + ":" + timeString[1])
+            shift.end = newTimeString
+
+        if "am" in shift.start:
+            timeString = shift.start
+            timeString = timeString.replace("am","")
+            shift.start = timeString
+        
+        if "am" in shift.end:
+            timeString = shift.end
+            timeString = timeString.replace("am","")
+            shift.end = timeString
+        
+
+    return listOfShiftObj
 
 def createCalendar(service, Name):
 
@@ -65,15 +100,15 @@ def deleteCalendar(service, IdString):
 #event = 
 def insertEvent(service, calendarId, startDate = '2020-12-29', endDate = '2020-12-29', startTime = '04:00:00-07:00', endTime = '5:00:00-07:00'):
   event = {
-    'summary': 'Final Exam',
+    'summary': 'GNC Shift',
   
-    'description': 'It\'s the exam',
+    'description': 'Work Shift',
     'start': {
-      'dateTime': startDate + 'T' + startTime,
+      'dateTime': startDate + 'T' + startTime+":00",
       'timeZone': 'America/New_York',
     },
     'end': {
-      'dateTime': '2020-12-29T5:00:00-07:00',
+      'dateTime': endDate + 'T' + endTime+":00",
       'timeZone': 'America/New_York',
     },
     'recurrence': [
@@ -92,9 +127,4 @@ def insertEvent(service, calendarId, startDate = '2020-12-29', endDate = '2020-1
     },
   }
 
-  service.events().insert(calendarId='mhj5hgpvpjs6g7po8pvj3r7q6c@group.calendar.google.com', body=event).execute()
-
-
-
-calID = createCalendar(service, "Final Dates")
-insertEvent(service, calID,startDate = '2020-12-29',endDate='2020-12-29',)
+  service.events().insert(calendarId= calendarId, body=event).execute()
